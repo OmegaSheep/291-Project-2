223fbe4df11c3a245f1f31a16dde1fc4106ba007
SuperSheep18 <nothingwillgowrong@gmail.com>
2014-11-20 16:10:37 -0700

    Added example code.

---
+++ Examples/a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import bsddb
+# Creating a database
+DATABASE = 'cstudents.db'
+
+# Making it with BTree organization
+db = bsddb.btopen(DATABASE,'c')
+
+# Inserting a record
+Sid = '123'
+Name = 'James'
+db[Sid] = Name
+
+#Obtaining the result for a query
+if (db.has_key(Sid)==True):
+	sname = db[Sid]
+	print(sname)
+
+# Deleting a record
+del db[Sid]
+db.sync()
+
+# Closing the connection
+db.close()
